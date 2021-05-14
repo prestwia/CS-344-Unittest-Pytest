d7572f25ef961a3952e83bd316d0f7f5876ebd22
Alex Prestwich <prestwia@oregonstate.edu>
2021-05-13 18:20:45 -0700

    completed implementation of palindrome module

---
+++ palindrome.py
@@ -0,0 +1,21 @@
+class Palindrome:
+    def palindrome(self, input):
+        if (type(input) is not str):
+            return "Invalid type, input must be string."
+        else:
+            if (len(input) % 2 == 0):
+                for i in range(int(len(input) / 2)):
+                    if (input[i] == input[len(input) - 1 - i]):
+                        continue
+                    else:
+                        return False
+            else:
+                for i in range(len(input) // 2):
+                    if (input[i] == input[len(input) - 1 - i]):
+                        continue
+                    else:
+                        return False
+            return True
+
+p1 = Palindrome()
+print(p1.palindrome(2))
